ff02ec33e58a7d2640bd3d87e5cfc74e814c7971
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 19:28:00 -0300

    feature: Adiciona tratamento para a exception BadRequestException

--- api/src/main/java/api/settings/GlobalExceptionHandler.java
+++ api/src/main/java/api/settings/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package api.settings;
 
 
 import api.dtos.responses.Response;
+import api.exceptions.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -27,5 +28,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new Response<>(null, null, errors));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<Response<Object>> handleBadRequestException(BadRequestException ex) {
+        log.error(ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new Response<>(null, null, List.of(ex.getMessage())));
     }
 }
