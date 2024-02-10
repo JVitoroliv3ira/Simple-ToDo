610c45cd28b55f0b1df13c631095c9cb4793418e
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 19:29:08 -0300

    feature: Utiliza os metodos do UserService para criar um novo usuario

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -1,6 +1,15 @@
 package api.controllers.v1;
 
+import api.dtos.requests.authentication.UserRegisterRequestDTO;
+import api.dtos.responses.Response;
+import api.models.User;
+import api.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/v1/auth")
 @RestController
 public class AuthController {
+    private final UserService userService;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<Response<User>> register(@Valid @RequestBody UserRegisterRequestDTO request) {
+        this.userService.validateEmailUniqueness(request.email());
+        User createdUser = this.userService.create(request.convert());
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new Response<>(
+                        createdUser,
+                        "Usuário cadastrado com sucesso",
+                        null
+                ));
+    }
 }
