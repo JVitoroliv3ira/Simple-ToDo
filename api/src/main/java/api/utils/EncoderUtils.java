ec62c925585b26603e812789b27e56a6d8849e01
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 20:50:07 -0300

    feature: Implementa utilitario para lidar com o encode de valores

---
+++ api/src/main/java/api/utils/EncoderUtils.java
@@ -0,0 +1,20 @@
+package api.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class EncoderUtils {
+    private final PasswordEncoder encoder;
+
+    public String encode(String rawValue) {
+        return this.encoder.encode(rawValue);
+    }
+
+    public Boolean matches(String rawValue, String encodedValue) {
+        return this.encoder.matches(rawValue, encodedValue);
+    }
+
+}
