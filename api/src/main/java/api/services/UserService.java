2e6c2f18c7b8276ae9ddffb6ef5d1d493ce15d37
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 19:28:30 -0300

    feature: Implementa metodo responsavel por validar a unicidade do email

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.exceptions.BadRequestException;
 import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository repository;
 
+    private static final String ERROR_EMAIL_NOT_UNIQUE = "O email informado já está sendo utilizado por outra conta";
+
     public User create(User entity) {
         entity.setId(null);
         return this.repository.save(entity);
+    }
+
+    public void validateEmailUniqueness(String email) {
+        if (Boolean.TRUE.equals(this.repository.existsByEmailIgnoreCase(email))) {
+            throw new BadRequestException(ERROR_EMAIL_NOT_UNIQUE);
+        }
     }
 }
