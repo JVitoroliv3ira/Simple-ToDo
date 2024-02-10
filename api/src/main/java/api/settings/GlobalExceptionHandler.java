84aeb214e3851231bdbdc75729dfb424d354d1ce
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 19:11:05 -0300

    feature: Implementa GlobalExceptionHandler para tratar as exceptions da api

---
+++ api/src/main/java/api/settings/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package api.settings;
+
+
+import api.dtos.responses.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Response<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error(ex.getMessage());
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        return ResponseEntity
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(new Response<>(null, null, errors));
+    }
+}
