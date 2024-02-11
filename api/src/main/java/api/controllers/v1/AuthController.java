ec9a0fe50151ee568e83ba64e08fbd4913ab1421
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-10 22:48:05 -0300

    feature: Implementa endpoint responsavel por autenticar um usuario

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -1,8 +1,11 @@
 package api.controllers.v1;
 
+import api.dtos.requests.authentication.UserAuthenticationRequestDTO;
 import api.dtos.requests.authentication.UserRegisterRequestDTO;
+import api.dtos.responses.AuthenticatedUserResponseDTO;
 import api.dtos.responses.Response;
 import api.models.User;
+import api.services.AuthService;
 import api.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthController {
     private final UserService userService;
+    private final AuthService authService;
 
     @PostMapping(path = "/register")
     public ResponseEntity<Response<User>> register(@Valid @RequestBody UserRegisterRequestDTO request) {
@@ -27,6 +31,18 @@ public class AuthController {
                 .body(new Response<>(
                         createdUser,
                         "Usuário cadastrado com sucesso",
+                        null
+                ));
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<Response<AuthenticatedUserResponseDTO>> login(@Valid @RequestBody UserAuthenticationRequestDTO request) {
+        AuthenticatedUserResponseDTO response = this.authService.authenticate(request.convert());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new Response<>(
+                        response,
+                        "Usuário autenticado com sucesso",
                         null
                 ));
     }
