395ca47787cf9dbd59af9514c0c530cb812c86dd
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-12 14:32:40 -0300

    feature: Implementa método criar um novo Todo

--- api/src/main/java/api/services/TodoService.java
+++ api/src/main/java/api/services/TodoService.java
@@ -1,9 +1,17 @@
 package api.services;
 
+import api.models.Todo;
+import api.repositories.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class TodoService {
+    private final TodoRepository repository;
+
+    public Todo create(Todo entity) {
+        entity.setId(null);
+        return this.repository.save(entity);
+    }
 }
