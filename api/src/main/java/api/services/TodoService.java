b6f580b169499dfe4612776df0e828a66ee318ae
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-12 16:26:16 -0300

    feature: Adiciona método responsável por buscar todo com base no id e no id do criador

--- api/src/main/java/api/services/TodoService.java
+++ api/src/main/java/api/services/TodoService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.exceptions.BadRequestException;
 import api.models.Todo;
 import api.repositories.TodoRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +11,17 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     private final TodoRepository repository;
 
+    private final String ERROR_INVALID_TODO_CREATOR = "Você não possui permissão para realizar esta ação";
+
     public Todo create(Todo entity) {
         entity.setId(null);
         entity.setIsFinished(false);
         return this.repository.save(entity);
+    }
+
+    public Todo findByIdAndUserId(Long id, Long userId) {
+        return this.repository
+                .findByIdAndUserId(id, userId)
+                .orElseThrow(() -> new BadRequestException(ERROR_INVALID_TODO_CREATOR));
     }
 }
