f499c7acccd17ccbc90bb1d9c3d30bbd44312441
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-12 12:50:10 -0300

    feature: Cria Controller para a entidade TOdo

---
+++ api/src/main/java/api/controllers/v1/TodoController.java
@@ -0,0 +1,11 @@
+package api.controllers.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/todo")
+@RestController
+public class TodoController {
+}
