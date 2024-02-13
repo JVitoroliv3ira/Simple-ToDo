899dc2f4533fceae8b03610311d9dbea947c7733
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-13 00:13:48 -0300

    feature: Adiciona novos métodos no TodoRepository

--- api/src/main/java/api/repositories/TodoRepository.java
+++ api/src/main/java/api/repositories/TodoRepository.java
@@ -4,9 +4,12 @@ import api.models.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Optional<Todo> findByIdAndUserId(Long id, Long userId);
+
+    Boolean existsByIdAndUserId(Long id, Long userId);
 }
