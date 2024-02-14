483f4e8dd838ea422b29fb8f4a829f05366b166c
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-14 18:21:54 -0300

    feature: Cria o main da página de registro

--- web/src/pages/authentication/register/index.tsx
+++ web/src/pages/authentication/register/index.tsx
@@ -9,6 +9,17 @@ const RegisterPage = () => {
         <a className="font-light" href="/app/auth/register">Crie sua conta</a>
         <a className="font-light" href="/app/auth/login">Entrar</a>
       </HeaderComponent>
+  
+      <main
+        className="
+          flex
+          items-center
+          justify-center
+          w-full
+          h-screen
+          bg-slate-100
+        ">
+      </main>
 
       <FooterComponent />
     </div>
