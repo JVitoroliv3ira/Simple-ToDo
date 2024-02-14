b1a9665468faa3f241d92badb9c2328aeab2e155
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-14 13:38:38 -0300

    feature: Implementa método responsável por buscar os todos utilizando paginacao

--- api/src/main/java/api/repositories/TodoRepository.java
+++ api/src/main/java/api/repositories/TodoRepository.java
@@ -1,15 +1,16 @@
 package api.repositories;
 
 import api.models.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Optional<Todo> findByIdAndUserId(Long id, Long userId);
-
     Boolean existsByIdAndUserId(Long id, Long userId);
+    Page<Todo> findAllByUserId(Long userId, Pageable pageable);
 }
