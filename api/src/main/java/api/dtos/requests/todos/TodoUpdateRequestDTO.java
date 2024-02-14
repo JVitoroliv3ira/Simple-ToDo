9d74ec8e2e11f230ea603ad22567b2f838972332
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-14 11:56:36 -0300

    feature: Implementa DTO responsável por transportar os dados da request de atualização de um Todo

---
+++ api/src/main/java/api/dtos/requests/todos/TodoUpdateRequestDTO.java
@@ -0,0 +1,36 @@
+package api.dtos.requests.todos;
+
+import api.models.Todo;
+import api.models.User;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record TodoUpdateRequestDTO(
+        @NotNull(message = "Informe a tarefa que você deseja atualizar")
+        Long id,
+        @NotNull(message = "O título não deve ser nulo")
+        @Size(min = 5, max = 60, message = "O título deve conter entre {min} e {max} caracteres")
+        String title,
+        @Size(max = 300, message = "A descrição deve conter no máximo {max} caracteres")
+        String description,
+        @NotNull(message = "Informe se a tarefa é uma prioridade")
+        Boolean isPriority,
+
+        @NotNull(message = "Informe a data de validade da tarefa")
+        LocalDate dueDate
+) {
+
+    public Todo convert(Long userId) {
+        return Todo
+                .builder()
+                .id(id())
+                .title(title())
+                .description(description())
+                .isPriority(isPriority())
+                .dueDate(dueDate())
+                .user(User.builder().id(userId).build())
+                .build();
+    }
+}
