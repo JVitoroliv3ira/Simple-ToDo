dd93a3719d6d11df9d4b30b54897e60e86baebcc
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-14 19:30:59 -0300

    feature: Implementa formulário de registro

--- web/src/pages/authentication/register/index.tsx
+++ web/src/pages/authentication/register/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HeaderComponent from "../../../components/header";
 import FooterComponent from "../../../components/footer";
 
@@ -18,7 +17,46 @@ const RegisterPage = () => {
           w-full
           h-screen
           bg-slate-100
+          p-10
         ">
+        
+        <div className="bg-slate-200 p-10 rounded shadow-lg w-full max-w-xl">
+          <form>
+            <div className="mb-8">
+              <label className="block mb-2 text-sm font-medium text-gray-950">
+                E-mail
+              </label>
+              <input
+                className="w-full p-3 rounded text-sm"
+                placeholder="Informe o seu endereço de e-mail"
+              />
+            </div>
+            <div className="mb-8">
+              <label className="block mb-2 text-sm font-medium text-gray-950">
+                Senha
+              </label>
+              <input
+                className="w-full p-3 rounded text-sm"
+                placeholder="Informe a sua senha"
+              />
+            </div>
+            <a href="/app/auth/login" className="font-light block mb-8">
+              Já possui uma conta?
+            </a>
+            <button
+              className="
+                w-full
+                p-3
+                rounded
+                bg-slate-950
+                hover:bg-slate-800
+                text-white"
+              >
+              Cadastrar
+            </button>
+          </form>
+        </div>
+
       </main>
 
       <FooterComponent />
