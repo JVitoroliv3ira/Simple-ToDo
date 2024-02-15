334dfc607d19010e3160f854964b5e631ce0c142
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-14 22:05:03 -0300

    feature: Implementa service para realizar o registro de usuário

---
+++ web/src/core/services/authentication/user-register.service.ts
@@ -0,0 +1,16 @@
+import UserAuthenticationRequestDTO from '../../dtos/requests/authentication/user-authentication-request.dto';
+import AuthenticatedUserRequestDTO from '../../dtos/responses/authentication/authenticated-user-request.dto';
+import ResponseDTO from '../../dtos/responses/response.dto';
+import apiService from '../api.service';
+
+const handleUserRegistration = (content: UserAuthenticationRequestDTO) => {
+  const BASE_URL = import.meta.env.VITE_SIMPLE_TODO_API_BASE_URL;
+  const endpoint =  `${BASE_URL}/auth/register`;
+  return apiService<UserAuthenticationRequestDTO, ResponseDTO<AuthenticatedUserRequestDTO>>(
+    endpoint,
+    'POST',
+    content
+  );
+}
+
+export default handleUserRegistration;
