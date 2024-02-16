52a6a241fc5e20ed05fd17fb062215ac055313ec
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-15 21:51:25 -0300

    feature: Cria funcoes utilitarias para manipular o localstorage

---
+++ web/src/core/utils/localstorage.util.ts
@@ -0,0 +1,20 @@
+const set = <C> (key: string, content: C): void => {
+  localStorage.setItem(key, JSON.stringify(content));
+}
+
+const get = <C> (key: string): C | null => {
+  const item = localStorage.getItem(key);
+  if (item === null || item === undefined) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(item);
+  } catch(e) {
+    return null;
+  }
+}
+
+const remove = (key: string): void => {
+  localStorage.removeItem(key);
+}
