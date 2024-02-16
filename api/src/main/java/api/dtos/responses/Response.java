4da4e33f31c8a8545763d413471a3ba0c9b570ce
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-15 21:50:45 -0300

    feature: Implementa metodo responsavel por retornar se a resposta da API possui erros

--- api/src/main/java/api/dtos/responses/Response.java
+++ api/src/main/java/api/dtos/responses/Response.java
@@ -1,6 +1,13 @@
 package api.dtos.responses;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.util.List;
+import java.util.Objects;
 
 public record Response<C>(C content, String message, List<String> errors) {
+    @JsonSerialize
+    public Boolean hasErrors() {
+        return Objects.nonNull(errors()) && !errors.isEmpty();
+    }
 }
