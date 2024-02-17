c9adaafda3cca32628302b544993cfeaa9d04525
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-17 14:35:19 -0300

    feature: Utiliza o guard na rota home

--- web/src/routes/routes.tsx
+++ web/src/routes/routes.tsx
@@ -3,6 +3,7 @@ import RegisterPage from '../pages/authentication/register';
 import { ToastContainer } from 'react-toastify';
 import LoginPage from '../pages/authentication/login';
 import HomePage from '../pages/todos/home';
+import AuthenticatedRouteGuard from './guards/authenticated-route.guard';
 
 const AppRouter = () => {
   return (
@@ -11,7 +12,11 @@ const AppRouter = () => {
       <Routes>
         <Route path="/app/auth/register" element={<RegisterPage />} />
         <Route path="/app/auth/login" element={<LoginPage />} />
-        <Route path="/app/home" element={<HomePage />} />
+        <Route path="/app/home" element={
+          <AuthenticatedRouteGuard redirectTo="/app/auth/login">
+            <HomePage />
+          </AuthenticatedRouteGuard>
+        } />
       </Routes>
     </Router>
   )
