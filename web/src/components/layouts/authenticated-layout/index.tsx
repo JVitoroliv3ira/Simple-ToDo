0abb13feebbe2ad666b4ad3240883e10f1404bab
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-17 14:54:10 -0300

    feature: Implementa layout para os usuarios autenticados

---
+++ web/src/components/layouts/authenticated-layout/index.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import HeaderComponent from '../../header';
+import FooterComponent from '../../footer';
+import { useNavigate } from 'react-router-dom';
+import { removeEmail, removeToken } from '../../../core/services/authentication/user-authentication.service';
+
+type AuthenticatedLayoutProps = {
+  children: ReactNode;
+};
+
+const AuthenticatedLayout = ({ children }: AuthenticatedLayoutProps) => {
+  const navigate = useNavigate();
+
+  const logoff = () => {
+    removeToken();
+    removeEmail();
+    navigate('/app/auth/login')
+  }
+
+  return (
+    <>
+      <HeaderComponent>
+        <button
+          onClick={logoff}
+          className="border-none bg-transparent text-sm font-light"
+        >
+          Sair
+        </button>
+      </HeaderComponent>
+      <main className="flex items-center justify-center w-full h-screen bg-slate-100 p-10">
+        {children}
+      </main>
+      <FooterComponent />
+    </>
+  );
+}
+
+export default AuthenticatedLayout;
