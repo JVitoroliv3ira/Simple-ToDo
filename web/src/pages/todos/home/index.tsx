7f80e2d7031b019adc33f54e559796e8f67674d1
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-18 01:56:06 -0300

    feature: Realiza testes com os novos componentes

--- web/src/pages/todos/home/index.tsx
+++ web/src/pages/todos/home/index.tsx
@@ -3,6 +3,8 @@ import CardHeaderComponent from "../../../components/card/card-header";
 import AuthenticatedLayout from "../../../components/layouts/authenticated-layout";
 
 import plusIcon from '../../../assets/icons/plus_icon.svg';
+import CardBodyComponent from "../../../components/card/card-body";
+import TodoDetailComponent from "../../../components/todo-detail";
 
 const HomePage = () => {
   return (
@@ -13,6 +15,30 @@ const HomePage = () => {
             <img src={plusIcon} alt="Cadastrar tarefa" />
           </button>
         </CardHeaderComponent>
+        
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
+        <CardBodyComponent>
+          
+          <TodoDetailComponent
+            title="Titulo 01"
+            description="Descricao 01"
+            isFinished={false}
+            isPriority={true}
+            deadlineHasPassed={true}
+            dueDate={new Date("2024-01-01")}
+          />
+
+          <TodoDetailComponent
+            title="Titulo 02"
+            description="Descricao 02"
+            isFinished={true}
+            isPriority={false}
+            deadlineHasPassed={false}
+            dueDate={new Date("2024-01-01")}
+          />
+
+        </CardBodyComponent>
       </CardComponent>
     </AuthenticatedLayout>
   )
