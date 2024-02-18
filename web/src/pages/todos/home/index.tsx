4d4efa5f01aa23f553c1c06e3c8e63c144f38154
João Oliveira <jvitoroliveira0711@gmail.com>
2024-02-17 23:31:50 -0300

    feature: Adiciona card para o crud de todos

--- web/src/pages/todos/home/index.tsx
+++ web/src/pages/todos/home/index.tsx
@@ -1,9 +1,19 @@
+import CardComponent from "../../../components/card";
+import CardHeaderComponent from "../../../components/card/card-header";
 import AuthenticatedLayout from "../../../components/layouts/authenticated-layout";
+
+import plusIcon from '../../../assets/icons/plus_icon.svg';
 
 const HomePage = () => {
   return (
     <AuthenticatedLayout>
-      <div></div>
+      <CardComponent>
+        <CardHeaderComponent title="Tarefas cadastradas">
+          <button className="p-1 bg-slate-950 rounded">
+            <img src={plusIcon} alt="Cadastrar tarefa" />
+          </button>
+        </CardHeaderComponent>
+      </CardComponent>
     </AuthenticatedLayout>
   )
 }
